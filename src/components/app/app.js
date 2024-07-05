b5b604849696bc37fd37bf4d233c9090adff995e
Anton Evdokimovich <r3d-adam@users.noreply.github.com>
2024-07-05 18:41:57 +0300

    Chore: remove  comments

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,14 +10,7 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Modal from '../modal/modal';
 
-// const API_URL = 'https://norma.nomoreparties.space/api/ingredients';
-
 const App = () => {
-	// const [state, setState] = useState({
-	// 	isLoading: false,
-	// 	hasError: false,
-	// });
-	// const [ingredients, setIngredients] = useState([]);
 	const dispatch = useDispatch();
 	const { ingredients, isLoading, error } = useSelector((store) => store.ingredients);
 
@@ -25,84 +18,7 @@ const App = () => {
 		dispatch(getIngredients());
 	}, []);
 
-	// const [order, setOrder] = useState([]);
 	const [page, setPage] = useState('Конструктор');
-
-	// useEffect(() => {
-	// 	setState({ ...state, hasError: false, isLoading: true });
-
-	// 	fetch(API_URL)
-	// 		.then((res) => {
-	// 			if (res.ok) {
-	// 				return res.json();
-	// 			} else {
-	// 				return Promise.reject(res);
-	// 			}
-	// 		})
-	// 		.then((data) => {
-	// 			setState({ ...state, isLoading: false });
-	// 			setIngredients(data.data);
-	// 		})
-	// 		.catch((e) => {
-	// 			console.log(e.message);
-	// 			setState({ ...state, hasError: true, isLoading: false });
-	// 		});
-	// }, []);
-
-	// useEffect(() => {
-	// 	generateRandomOrder({});
-	// }, [ingredients]);
-
-	// const generateRandomOrder = ({ maxIngredients = 10, chance = 0.2, maxSameIngredient = 2 }) => {
-	// 	if (ingredients.length && !order.length) {
-	// 		const ingredientsCopy = [...ingredients];
-
-	// 		const bunList = ingredientsCopy.filter((ingredient) => ingredient.type === 'bun');
-
-	// 		const bun = bunList[getRandomInt(0, bunList.length - 1)];
-	// 		const order = [{ ...bun, count: 1 }];
-	// 		let totalIngredients = 1;
-
-	// 		ingredientsCopy
-	// 			.filter((ingredient) => ingredient.type !== 'bun')
-	// 			.forEach((item, i) => {
-	// 				if (getRandomInt(0, 100) <= chance * 100 && totalIngredients < maxIngredients) {
-	// 					let count = getRandomInt(1, maxSameIngredient);
-	// 					if (totalIngredients + count > maxIngredients) {
-	// 						count = maxIngredients - totalIngredients;
-	// 					}
-	// 					totalIngredients += count;
-	// 					order.push({ ...item, count });
-	// 				}
-	// 			});
-
-	// 		setOrder(order);
-	// 	}
-	// };
-
-	// const updateOrder = (ingredient) => {
-	// 	const index = order.findIndex((item) => item._id === ingredient._id);
-
-	// 	let newOrder = [...order];
-
-	// 	if (index !== -1) {
-	// 		if (ingredient.count <= 0) {
-	// 			newOrder.splice(index, 1);
-	// 		} else {
-	// 			newOrder[index] = {
-	// 				...ingredient,
-	// 				count: ingredient.type === 'bun' ? 1 : ingredient.count,
-	// 			};
-	// 		}
-	// 	} else {
-	// 		if (ingredient.type === 'bun') {
-	// 			newOrder = newOrder.filter((item) => item.type !== 'bun');
-	// 		}
-	// 		newOrder.push({ ...ingredient, count: 1 });
-	// 	}
-
-	// 	setOrder(newOrder);
-	// };
 
 	return (
 		<>
@@ -114,8 +30,6 @@ const App = () => {
 				<main className={styles.mainContainer}>
 					<DndProvider backend={HTML5Backend}>
 						<BurgerIngredients />
-						{/* <BurgerIngredients order={order} updateOrder={updateOrder} /> */}
-						{/* <BurgerConstructor order={order} updateOrder={updateOrder} /> */}
 						<BurgerConstructor />
 					</DndProvider>
 				</main>
